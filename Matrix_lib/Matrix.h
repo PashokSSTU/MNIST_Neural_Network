b834a7f0b9d24ba1004cc9b1d33dda6d47ec31d6
DESKTOP-BC722G9\ga <pavel@generalarcade.com>
2022-07-07 16:23:52 +0400

    Update matrix.lib

--- Matrix_lib/Matrix.h
+++ Matrix_lib/Matrix.h
@@ -47,81 +47,11 @@ class Matrix
 {
 public:
 	
-	Matrix(unsigned int rows = 3, unsigned int columns = 3) : size({ rows, columns })
-	{
-		matrix.resize(rows);
-
-		for (int j = 0; j < rows; j++)
-		{
-			matrix[j].resize(columns);
-		}
-	}
-
-	Matrix(matrix_size_t _size) : size(_size)
-	{
-		matrix.resize(_size.rows);
-
-		for (int j = 0; j < _size.rows; j++)
-		{
-			matrix[j].resize(_size.columns);
-		}
-	}
-
-	Matrix(const Matrix& _matrix)
-	{
-		if (this->size != _matrix.size)
-		{
-			this->size = _matrix.size;
-
-			matrix.resize(_matrix.size.rows);
-
-			for (int i = 0; i < _matrix.size.rows; i++)
-			{
-				matrix[i].resize(_matrix.size.columns);
-			}
-
-			for (int i = 0; i < size.rows; i++)
-			{
-				for (int j = 0; j < size.columns; j++)
-				{
-					matrix[i][j] = _matrix.matrix[i][j];
-				}
-			}
-		}
-		else
-		{
-			for (int i = 0; i < size.rows; i++)
-			{
-				for (int j = 0; j < size.columns; j++)
-				{
-					matrix[i][j] = _matrix.matrix[i][j];
-				}
-			}
-		}
-	}
-
-	Matrix(const std::initializer_list<std::initializer_list<double>>& list) : size({ list.size(), 0 })
-	{
-		for (auto& x : list)
-			if (x.size() > size.columns)
-				size.columns = x.size();
-		
-		matrix.resize(size.rows);
-		for (int i = 0; i < size.rows; i++)
-		{
-			matrix[i].resize(size.columns);
-		}
-
-		auto it = list.begin();
-		for (int i = 0; i < size.rows; i++, it++)
-		{
-			std::copy(it->begin(), it->end(), matrix[i].begin());
-		}
-	}
-
-	~Matrix()
-	{
-	}
+	Matrix(unsigned int rows = 3, unsigned int columns = 3);
+	Matrix(matrix_size_t _size);
+	Matrix(const Matrix& _matrix);
+	Matrix(const std::initializer_list<std::initializer_list<double>>& list);
+	~Matrix();
 
 	void resize(matrix_size_t size);
 	void resize(unsigned int rows, unsigned int columns);
